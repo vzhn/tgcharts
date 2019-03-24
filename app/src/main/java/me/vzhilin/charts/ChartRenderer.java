fc193bd9bd1cda7725a8fe37061929b327988c70
vzhilin <zhilin.vladimir.nic@gmail.com>
2019-03-24 21:26:32 +0300

    removed comments

--- app/src/main/java/me/vzhilin/charts/ChartRenderer.java
+++ app/src/main/java/me/vzhilin/charts/ChartRenderer.java
@@ -31,24 +31,11 @@ public class ChartRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onDrawFrame(GL10 unused) {
-        // Redraw background color
         GLES31.glClear(GLES31.GL_COLOR_BUFFER_BIT);
-
-        // Set the camera position (View matrix)
         Matrix.setLookAtM(mViewMatrix, 0, 0, 0, +6, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
-
-        // Create a rotation and translation for the cube
         Matrix.setIdentityM(mRotationMatrix, 0);
-
         Matrix.translateM(mRotationMatrix, 0, 0, 0, 0);
-
-        //Assign mRotationMatrix a rotation with the seekbar
-//        Matrix.rotateM(mRotationMatrix, 0, zoom * 3.6f, 1.0f, 1.0f, 1.0f);
-
-        // combine the model with the view matrix
         Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mRotationMatrix, 0);
-
-        // combine the model-view with the projection matrix
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
 
         boolean dirty = false;
